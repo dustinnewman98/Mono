e5be65a1b7d9d1aeb72b04fb6c5d95196523e440
Dustin Newman <dustinnewman98@gmail.com>
2018-10-22 14:19:28 -0700

    remove package lock

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,53 +1,86 @@
-import React from "react";
+import React from 'react';
 import Link from 'gatsby-link';
-import PageHeader from "../components/page-header";
-import PageContent from "../components/page-content";
+import PageHeader from '../components/page-header';
+import PageContent from '../components/page-content';
 
 function getMonth(input) {
-    let d = new Date(input);
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    return months[d.getMonth()];
+  let d = new Date(input);
+  let months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  return months[d.getMonth()];
 }
 
 function getDay(input) {
-    let d = new Date(input);
-    return d.getDate();
+  let d = new Date(input);
+  return d.getDate();
 }
 
 export default function Posts({ data }) {
-    const { edges: posts } = data.allMarkdownRemark;
-    return (
-        <PageContent>
-            <PageHeader text='My Posts' emoji='💌' />
-            {posts
-                .filter(post => post.node.frontmatter.title.length > 0)
-                .map(({ node: post }) => {
-                    return (<Link to={post.fields.path} className="one-post" style={{ display: 'flex' }}>
-                        <div className="one-post-date" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                            <h1 className="one-post-date-day">{getDay(post.fields.date)}</h1>
-                            <p className="one-post-date-month">{getMonth(post.fields.date)}</p>
-                        </div>
-                        <div className="one-post-blurb" style={{ display: 'flex', flexDirection: 'column' }}>
-                            <h1 className="one-post-blurb-title">{post.frontmatter.title}</h1>
-                            <h2 className="one-post-blurb-subtitle">{post.frontmatter.subtitle}</h2>
-                        </div>
-                    </Link>);
-                })}
-        </PageContent>
-    );
-};
+  const { edges: posts } = data.allMarkdownRemark;
+  return (
+    <PageContent>
+      <PageHeader text="My Posts" emoji="💌" />
+      {posts
+        .filter(post => post.node.frontmatter.title.length > 0)
+        .map(({ node: post }) => {
+          return (
+            <Link
+              to={post.fields.path}
+              className="one-post"
+              style={{ display: 'flex' }}
+            >
+              <div
+                className="one-post-date"
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}
+              >
+                <h1 className="one-post-date-day">
+                  {getDay(post.fields.date)}
+                </h1>
+                <p className="one-post-date-month">
+                  {getMonth(post.fields.date)}
+                </p>
+              </div>
+              <div
+                className="one-post-blurb"
+                style={{ display: 'flex', flexDirection: 'column' }}
+              >
+                <h1 className="one-post-blurb-title">
+                  {post.frontmatter.title}
+                </h1>
+                <h2 className="one-post-blurb-subtitle">
+                  {post.frontmatter.subtitle}
+                </h2>
+              </div>
+            </Link>
+          );
+        })}
+    </PageContent>
+  );
+}
 
-export const pageQuery = graphql`
-  query IndexQuery {
+export const postQuery = graphql`
+  query postQuery {
     allMarkdownRemark(
         filter: {
             sourceInstanceName: { 
                 eq: "posts" 
             }
-        }
-        sort: { 
-            order: DESC, 
-            fields: [fields___date] 
         }
     ) {
       edges {
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,32 +1,35 @@
-import React from "react";
-import PageHeader from "../components/page-header";
-import PageContent from "../components/page-content";
-import Project from "../components/project";
+import React from 'react';
+import PageHeader from '../components/page-header';
+import PageContent from '../components/page-content';
+import Project from '../components/project';
 
 export default function Projects() {
-    const { edges: projects } = data.allMarkdownRemark;
+  const { edges: projects } = data.allMarkdownRemark;
 
-    return (
-        <PageContent>
-            <PageHeader text='My Projects' emoji='🤖' />
-            {projects
-                .filter(project => project.node.frontmatter.title.length > 0)
-                .map(({ node: project }) => {
-                    return <Project 
-                                title={project.frontmatter.title}
-                                description={project.frontmatter.description}
-                                role={project.frontmatter.role}
-                                stack={project.frontmatter.stack}
-                                github={project.frontmatter.github}
-                                live={project.frontmatter.live}/>
-                })}
-        </PageContent>
-    );
-}; 
+  return (
+    <PageContent>
+      <PageHeader text="My Projects" emoji="🤖" />
+      {projects
+        .filter(project => project.node.frontmatter.title.length > 0)
+        .map(({ node: project }) => {
+          return (
+            <Project
+              title={project.frontmatter.title}
+              description={project.frontmatter.description}
+              role={project.frontmatter.role}
+              stack={project.frontmatter.stack}
+              github={project.frontmatter.github}
+              live={project.frontmatter.live}
+            />
+          );
+        })}
+    </PageContent>
+  );
+}
 
 export const projQuery = graphql`
-  query IndexQuery {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+  query projQuery {
+    allMarkdownRemark {
       edges {
         node {
           excerpt(pruneLength: 250)
